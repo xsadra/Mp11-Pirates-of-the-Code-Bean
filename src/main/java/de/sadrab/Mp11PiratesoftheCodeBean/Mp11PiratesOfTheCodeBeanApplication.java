b86c888364d90a736772501fa96822939a766693
Sadra <ti9mail@gmail.com>
2018-04-16 09:09:21 +0200

    Add Spring structure

---
+++ src/main/java/de/sadrab/Mp11PiratesoftheCodeBean/Mp11PiratesOfTheCodeBeanApplication.java
@@ -0,0 +1,12 @@
+package de.sadrab.Mp11PiratesoftheCodeBean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp11PiratesOfTheCodeBeanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp11PiratesOfTheCodeBeanApplication.class, args);
+	}
+}
